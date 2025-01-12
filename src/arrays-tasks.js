b0283b8bf2c46afae92d160bc34aeeea1e405f77
BjornHola <broadherstby@gmail.com>
2025-01-12 17:38:26 +0300

    init: fulfil task get the greater numbers from array according to n

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -599,10 +599,14 @@ console.log(getHexRGBValues([]));
  *   getMaxItems([ 10, 2, 7, 5, 3, -5 ], 3) => [ 10, 7, 5 ]
  *   getMaxItems([ 10, 10, 10, 10 ], 3) => [ 10, 10, 10 ]
  */
-function getMaxItems(/* arr, n */) {
-  throw new Error('Not implemented');
+function getMaxItems(arr, n) {
+  return arr.sort((a, b) => b - a).slice(0, n);
 }
-
+console.log(getMaxItems([], 5));
+console.log(getMaxItems([1, 2], 1));
+console.log(getMaxItems([2, 3, 1], 2));
+console.log(getMaxItems([10, 2, 7, 5, 3, -5], 3));
+console.log(getMaxItems([10, 10, 10, 10], 3));
 /**
  * Finds and returns an array containing only the common elements found in two arrays.
  *
