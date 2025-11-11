cd953cf22c410cfbb46b318eac32f58d0769f898
BjornHola <broadherstby@gmail.com>
2025-11-11 12:55:48 +0300

    task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -141,10 +141,14 @@ getStringsLength(['', 'a', 'bc', 'def', 'ghij']);
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((acc, val) => {
+    return acc + val;
+  }, 0);
+  return Number((sum / arr.length).toFixed(2));
 }
-
+getAverage([1, 2, 3]);
 /**
  * Checks if all strings in an array have the same length.
  *
